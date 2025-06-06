6563c4a1fba3d8fc6c37fddacf8134992be190c5
AHMED <ahmedhassanhashem@outlook.com>
2025-06-07 02:51:40 +0300

    add cors to spring security config with custom defaults

--- src/main/java/gov/iti/jet/ewd/ecom/config/SecurityConfig.java
+++ src/main/java/gov/iti/jet/ewd/ecom/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+	.cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 //  Use session-based authentication instead of stateless
                 .sessionManagement(session -> session
